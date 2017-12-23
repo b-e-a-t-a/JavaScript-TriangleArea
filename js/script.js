a110f8566a11b7c146f9c4c28b428f51fd1c781d
Beata Karolczak <karolczak.beata@gmail.com>
2017-12-23 23:15:48 +0100

    Add Triangle Area exercise

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* var a = 2,
+  h = 5, 
+  var triangleArea = a*h/2; 
+  console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+  */
+  
+
+var a = prompt('Please specify base a:'),
+  h = prompt('Please specify height h:'),
+  triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
